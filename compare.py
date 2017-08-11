ca7e99c4221ccc81b0b5ede86a0ae34c604e2b95
Stephan Mertens <steph_cs@gmx.de>
2017-08-11 18:11:55 +0200

    now with serchbeer function

--- compare.py
+++ compare.py
@@ -1,47 +1,69 @@
+#-*-coding:utf8;-*-
+#qpy:console
+#qpy:2
+
 import pickle
 import os
+from termcolor import colored
 
-## Load and sort data
-while True:
-    available = []
-    inDir = os.listdir("./")
-    for i in inDir:
-        if i[-8:] == ".ratings":
-            available.append(i[0:-8])
 
-    amount = 2 #input("Shopping for how many Ratebeerians? ")
-    # try: amount = int(amount)
-    # except: print("Please insert an integer ")
-    # users = []
-    # k = 0
-    # for i in range(0, amount):
-    #     k += 1
-    #     while True:
-    #         user = input("Username #" + str(k) + ": ")
-    #         if isinstance(user, str):
-    #             if user in available:
-    #                 if user not in users:
-    #                     users.append(user)
-    #                     break
-    #                 else:
-    #                     print("User already in list.")
-    #                     while True:
-    #                         answer = input("Do you want to reduce the number of ratebeerians by 1? (Y/N)")
-    #                         if answer.lower() == "y":
-    #                             amount -= 1
-    #                             k -= 1
-    #                             break
-    #                         elif answer.lower() == "n":
-    #                             k -= 1
-    #                             break
-    #                     break
-    #             else: print("Error: Typo in username or ratings are not available. Please try again. ")
-    break
-users = ["neur0", "jfb"]
-ratings = []
-for i in users: ratings.append(i.split())
-for i in range(0, len(users)):
-    with open((users[i] + ".ratings"), "rb") as f: ratings[i].append(pickle.load(f))
+def loaddata():
+    ## Load and sort data
+    # while True:
+    #     available = []
+    #     inDir = os.listdir("./")
+    #     for i in inDir:
+    #         if i[-8:] == ".ratings":
+    #             available.append(i[0:-8])
+    #
+    #     amount = 2 #input("Shopping for how many Ratebeerians? ")
+        # try: amount = int(amount)
+        # except: print("Please insert an integer ")
+        # users = []
+        # k = 0
+        # for i in range(0, amount):
+        #     k += 1
+        #     while True:
+        #         user = input("Username #" + str(k) + ": ")
+        #         if isinstance(user, str):
+        #             if user in available:
+        #                 if user not in users:
+        #                     users.append(user)
+        #                     break
+        #                 else:
+        #                     print("User already in list.")
+        #                     while True:
+        #                         answer = input("Do you want to reduce the number of ratebeerians by 1? (Y/N)")
+        #                         if answer.lower() == "y":
+        #                             amount -= 1
+        #                             k -= 1
+        #                             break
+        #                         elif answer.lower() == "n":
+        #                             k -= 1
+        #                             break
+        #                     break
+        #             else: print("Error: Typo in username or ratings are not available. Please try again. ")
+        # break
+    users = ["neur0", "jfb"]
+    ratings = []
+    for i in users: ratings.append(i.split())
+    for i in range(0, len(users)):
+        with open((users[i] + ".ratings"), "rb") as f: ratings[i].append(pickle.load(f))
+    return ratings
+
+
+def searchbeer(userratings, srch):
+    # for i in userratings:
+    #     if queue in i:
+    #         print(colored(i, 'red'))
+
+    fnd = []
+    for i in userratings:
+        if srch.lower() in i.lower():
+            fnd.append(i)
+    return fnd
+
+ratings = loaddata()
 
 print("---------------------------------------------------------\n"
       "| Start shopping - Enter Beer Name - Enter exit to quit |\n"
@@ -50,7 +72,10 @@ while True:
     search = input("> ")
     if search == "exit":
         break
-    for i in ratings:
-        for j in i[1]:
-            if search.lower() in j.lower():
-                print(i[0] + ": " + j)
+    found = []
+    colors = ['red', 'green', 'blue']
+    for i in range(0, len(ratings)):
+        temp = searchbeer(ratings[i][1], search)
+        found.append(temp)
+        print(colored(found[i], colors[i]))
+
