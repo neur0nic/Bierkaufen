34266b885ab086de7774a611749de166609f8055
Stephan Mertens <steph_cs@gmx.de>
2017-08-13 01:13:26 +0200

    forgot to uncomment stuff

--- compare.py
+++ compare.py
@@ -9,43 +9,46 @@ import platform
 
 
 def loaddata():
-    ## Load and sort data
-    # while True:
-    #     available = []
-    #     inDir = os.listdir(wdir)
-    #     for i in inDir:
-    #         if i[-8:] == ".ratings":
-    #             available.append(i[0:-8])
-    #
-    #     amount = 2 #input("Shopping for how many Ratebeerians? ")
-        # try: amount = int(amount)
-        # except: print("Please insert an integer ")
-        # users = []
-        # k = 0
-        # for i in range(0, amount):
-        #     k += 1
-        #     while True:
-        #         user = input("Username #" + str(k) + ": ")
-        #         if isinstance(user, str):
-        #             if user in available:
-        #                 if user not in users:
-        #                     users.append(user)
-        #                     break
-        #                 else:
-        #                     print("User already in list.")
-        #                     while True:
-        #                         answer = input("Do you want to reduce the number of ratebeerians by 1? (Y/N)")
-        #                         if answer.lower() == "y":
-        #                             amount -= 1
-        #                             k -= 1
-        #                             break
-        #                         elif answer.lower() == "n":
-        #                             k -= 1
-        #                             break
-        #                     break
-        #             else: print("Error: Typo in username or ratings are not available. Please try again. ")
-        # break
-    users = ["neur0", "jfb"]
+    # Load and sort data
+    while True:
+        available = []
+        machine = platform.machine()
+        wdir = ("./")
+        if (machine == 'armv7l'):
+            wdir = ('/storage/emulated/0/qpython/scripts3')
+        inDir = os.listdir(wdir)
+        for i in inDir:
+            if i[-8:] == ".ratings":
+                available.append(i[0:-8])
+
+        amount = 2 #input("Shopping for how many Ratebeerians? ")
+        try: amount = int(amount)
+        except: print("Please insert an integer ")
+        users = []
+        k = 0
+        for i in range(0, amount):
+            k += 1
+            while True:
+                user = input("Username #" + str(k) + ": ")
+                if isinstance(user, str):
+                    if user in available:
+                        if user not in users:
+                            users.append(user)
+                            break
+                        else:
+                            print("User already in list.")
+                            while True:
+                                answer = input("Do you want to reduce the number of ratebeerians by 1? (Y/N)")
+                                if answer.lower() == "y":
+                                    amount -= 1
+                                    k -= 1
+                                    break
+                                elif answer.lower() == "n":
+                                    k -= 1
+                                    break
+                            break
+                    else: print("Error: Typo in username or ratings are not available. Please try again. ")
+        break
     ratings = []
     machine = platform.machine()
     wdir = ("./")
